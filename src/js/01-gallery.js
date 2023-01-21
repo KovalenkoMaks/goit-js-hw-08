a627a2270d61785483a62899b3a2d0863a666083
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-21 20:42:39 +0200

    add task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.js';
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = galleryItems
+  .map(
+    element =>
+      `<a href="${element.original}">
+      <img
+      src="${element.preview}" 
+      alt=""
+      title="${element.description}"/>
+      </a>`
+  )
+  .join('');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
