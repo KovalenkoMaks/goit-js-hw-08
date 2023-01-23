16dc9cd1586379779f41a25c12098d1f049ce957
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-23 22:31:21 +0200

    add task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const emailInputEl = document.querySelector('input');
+const mesgInputEl = document.querySelector('textarea');
+const formEl = document.querySelector('form');
+const formBtn = document.querySelector('button');
+const STORAGE_KEY = 'feedback-form-state';
+populateForm();
+
+formEl.addEventListener(
+  'input',
+  throttle(() => {
+    localStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify({
+        email: formEl.email.value,
+        message: formEl.message.value,
+      })
+    );
+  }, 500)
+);
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  formEl.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
+
+function populateForm() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    emailInputEl.value = savedData.email.trim();
+    mesgInputEl.value = savedData.message.trim();
+  }
+}
