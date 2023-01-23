96cc8f01af5a0fd04112bc4e3ad1f6eecd64355b
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-23 15:45:44 +0200

    add task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// const iframe = document.querySelector('#vimeo-player');
+const iframe = document.querySelector('iframe');
+// console.log(iframe);
+const player = new Player(iframe);
 
+player.on(
+  'timeupdate',
+  throttle(function (e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
